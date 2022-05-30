6fb21b0e713a496db2fff7cc47ccbd169ce8edf0
faaris <faaris@Kappa>
2022-05-30 15:59:39 +0100

    Remove unnecessary scripts and clean up Picom Config

--- scripts/catpush.sh
+++
@@ -1,4 +0,0 @@
-cd "$HOME"/packages/catppuccin-factory || exit
-git add -A && git commit -m 'changes.'
-git push -u -f origin master 
-
--- scripts/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/.dots
-pacman -Qqet > packages.list
-git add -A && git commit -m 'Changes'
-git push -u -f origin main
